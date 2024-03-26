2e11f99df5e16cf43942fd90d410739915228f7e
Michael Hassett <mhassett@iinet.net.au>
2024-03-26 22:21:34 +1100

    Modified models to be classes

--- peak_fitting.py
+++ peak_fitting.py
@@ -6,73 +6,61 @@ from astropy.modeling.models import Gaussian1D
 
 class CubicIceModel:
     def __init__(self, amplitude, mean, stddev):
-        self.init_amplitude = amplitude
-        self.init_mean = mean
-        self.init_stddev = stddev
-        self.init_model = Gaussian1D(amplitude=10, mean=12, stddev=5, name='cubic')
         self.fitter = LevMarLSQFitter()
+        self.fitted_model = None
+        self.model = self.__init_model__(amplitude, mean, stddev)
+
+    def __init_model__(self, amplitude, mean, stddev):
+        return Gaussian1D(amplitude=amplitude, mean=mean, stddev=stddev, name='cubic')
+
+    def fit(self, x_data, y_data):
+        self.fitted_model = self.fitter(self.model, x_data, y_data)
+        self.__get_models__()
+
+    def __get_models__(self):
+        return self.fitted_model
 
 
-class HexIceModel:
+class HexIceModel(CubicIceModel):
     def __init__(self,
                  amplitudes: tuple[int, int, int],
                  means: tuple[int, int, int],
                  stddevs: tuple[int, int, int]):
-        self.init_amplitude = amplitudes
-        self.init_mean = means
-        self.init_stddev = stddevs
+        super().__init__(amplitudes, means, stddevs)
 
-        self.amplitudes = [None, None, None]
-        self.means = [None, None, None]
-        self.stddevs = [None, None, None]
-        self.fwhm = [None, None, None]
+    def __init_model__(self, amplitude: tuple[int, int, int],
+                       mean: tuple[int, int, int],
+                       stddev: tuple[int, int, int]):
+        self.hex_1 = Gaussian1D(amplitude=amplitude[0], mean=mean[0], stddev=stddev[0],
+                                name='hex_1')
+        self.hex_2 = Gaussian1D(amplitude=amplitude[1], mean=mean[1], stddev=stddev[1],
+                                name='hex_2')
+        self.hex_3 = Gaussian1D(amplitude=amplitude[2], mean=mean[2], stddev=stddev[2],
+                                name='hex_3')
+        return self.hex_1 + self.hex_2 + self.hex_3
 
-        hex_1 = Gaussian1D(amplitude=self.init_amplitude[0], mean=self.init_mean[0], stddev=self.init_stddev[0],
-                           name='hex_1')
-        hex_2 = Gaussian1D(amplitude=self.init_amplitude[1], mean=self.init_mean[1], stddev=self.init_stddev[1],
-                           name='hex_2')
-        hex_3 = Gaussian1D(amplitude=self.init_amplitude[2], mean=self.init_mean[2], stddev=self.init_stddev[2],
-                           name='hex_3')
-        self.init_model = hex_1 + hex_2 + hex_3
-        self.fitter = LevMarLSQFitter()
-        self.fitted_model = None
-
-    def fit(self, x_data, y_data):
-        self.fitted_model = self.fitter(self.init_model, x_data, y_data)
-        self.__get_amplitudes__()
-
-    def __get_amplitudes__(self):
-        hex_1_model = self.fitted_model['hex_1']
-        hex_2_model = self.fitted_model['hex_2']
-        hex_3_model = self.fitted_model['hex_3']
-        for i, model in enumerate([hex_1_model, hex_2_model, hex_3_model]):
-            self.amplitudes[i] = model.amplitude
-            self.means[i] = model.mean
-            self.stddevs[i] = model.stddev
-            self.fwhm[i] = model.fwhm
+    def __get_models__(self):
+        self.hex_1 = self.fitted_model['hex_1']
+        self.hex_2 = self.fitted_model['hex_2']
+        self.hex_3 = self.fitted_model['hex_3']
 
 
 def gaussian_fitting():
-    m = Gaussian1D(amplitude=10, mean=10, stddev=5) + Gaussian1D(amplitude=10, mean=50, stddev=5) + Gaussian1D(
+    m = Gaussian1D(amplitude=10, mean=20, stddev=5) + Gaussian1D(amplitude=10, mean=50, stddev=5) + Gaussian1D(
         amplitude=10, mean=80, stddev=5)
-    x = np.linspace(0, 200, 2000)
+    x = np.linspace(0, 100, 2000)
     data = m(x)
     data = data + np.sqrt(data) * (np.random.random(x.size) - 0.5)
     # data -= data.min()
     plt.plot(x, data)
 
-    HexIce = HexIceModel(amplitudes=[10, 10, 10], means=[10, 50, 80], stddevs=[5, 5, 5])
+    HexIce = HexIceModel(amplitudes=[15, 8, 12], means=[15, 52, 79], stddevs=[4, 5, 6])
     HexIce.fit(x, data)
 
-    print('Amplitudes')
-    for amplitude in HexIce.amplitudes:
-        print(amplitude)
+    print(f'Mean: {HexIce.hex_1.mean.value}, Amplitude: {HexIce.hex_1.amplitude.value}, FWHM: {HexIce.hex_1.fwhm}')
+    print(f'Mean: {HexIce.hex_2.mean.value}, Amplitude: {HexIce.hex_2.amplitude.value}, FWHM: {HexIce.hex_2.fwhm}')
+    print(f'Mean: {HexIce.hex_3.mean.value}, Amplitude: {HexIce.hex_3.amplitude.value}, FWHM: {HexIce.hex_3.fwhm}')
 
-    print('FWHM')
-    for fwhm in HexIce.fwhm:
-        print(fwhm)
-
-    plt.plot(x, data)
     plt.plot(x, HexIce.fitted_model(x))
     plt.show()
 
