95a80baeea43768b08a1634954a8637e1fcf2f23
mph759 <mhassett@iinet.net.au>
2024-03-29 18:11:43 +0900

    Obtained test data and created initial framework for io.py to ingest and pre-process the data

---
+++ test.py
@@ -0,0 +1,16 @@
+"""
+Ice characterisation (cubicity and domain size) from XFEL diffraction data
+Written for use at PAL-XFEL experiment 2023-2nd-XSS-040
+Created on 2024-03-26 by Michael Hassett
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from ice_peak_predict import IcePeakPrediction
+from peak_fitting import CubicIceFitting, HexIceFitting
+from cubicity_char import normalise_peaks, cubicity
+from domain_size_char import domain_size
+
+if __name__ == '__main__':
+    wavelength = 15 # keV
+    ice_reference = IcePeakPrediction()
